33e07efee5f82f0bc780f66067477cb52604a0b0
icquesadaj@ufpso.edu.co <ejaimesp@ufpso.edu.co>
2023-03-07 21:11:50 -0500

    implementar arquitectura y entidad de bd

--- src/main/java/com/procesos/inventario/InventarioApplication.java
+++ src/main/java/com/procesos/inventario/InventarioApplication.java
@@ -3,6 +3,8 @@ package com.procesos.inventario;
 import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 @Data
 
 @SpringBootApplication
